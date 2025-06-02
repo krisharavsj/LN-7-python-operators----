0db8fc74a9143728a2a90aa720e6f1586a87c172
krisharavsj <krisharsv@gmail.com>
2025-06-02 09:34:26 +0530

    identity operator.py

---
+++ identity operator.py
@@ -0,0 +1,15 @@
+x = 5
+if(type(x)is int):
+ print("x is an integer")
+else:
+ print("no x is not an integer")
+ x=5.0
+if (type(x)is float):
+  print("x is an float")
+else:
+  print("x is not a float")
+  y=30
+if x is not y:
+  print ("x and y are not the same")
+else:
+  print("x and y are the same")
